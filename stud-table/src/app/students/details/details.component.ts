184cdf4c99b389f0b6e6db54378a7aeee8840a59
Sasidharan SD <47988127+aerosasi@users.noreply.github.com>
2020-02-06 15:42:46 +0530

    Another method of add and update
    Used another method to add and update records in the angular class .

--- stud-table/src/app/students/details/details.component.ts
+++ stud-table/src/app/students/details/details.component.ts
@@ -10,71 +10,73 @@ import { Form, NgForm } from '@angular/forms';
 })
 export class DetailsComponent implements OnInit {
  
-  show: boolean = true;
+  update: boolean = false;
   studata:studata[];
   studname;
   studgender;
   studDept;
   studDesc;
-  rid=1;
+  rid=101;  //Change this if changing initial record
   uid;
-  
+
   constructor() {
  this.studata=[{
-    id:1,
+    id:101,
     name:"ram",
     gender:"Male",
     dept:"IT",
     desc:"Good in Data Science and Handball"
   }]
-  
-
-   }
+}
 
   ngOnInit() {
-
-   
   }
 
- addStudent(){
+ addStudent(form:NgForm){
   console.log(this.studname + this.studgender);
+  console.log(this.update);
+  
+   if(this.update==false)   //Insertion part
+   {
     this.studata.push({
       id:this.rid+1,
       name: this.studname,
       gender: this.studgender,
       dept:this.studDept,
-      desc:this.studDesc 
+      desc:this.studDesc
+     
     })
     this.rid++;
+    form.reset(); 
+  }
+  else    // Update Part
+  {
+    this.update=false;
+    this.studata[this.uid-1].name=this.studname;
+    this.studata[this.uid-1].gender=this.studgender;
+    this.studata[this.uid-1].desc=this.studDesc;
+    this.studata[this.uid-1].dept=this.studDept;
     console.log(this.studata);
+     form.reset();
+  }
+ 
  }
 
  deleteStudent(id: number): void {
+   //deletion part
   this.studata = this.studata.filter(studata => studata.id !== id);
 }
 
-updateStudent(id:number,name:string,gender:string,dept:string,desc:String){
+
+//Displaying records to be updated
+updateStudent(id:number,name:string,gender:string,dept:string,desc:String){  
   
- 
-  this.show=false;
+  this.update=true;  //this is a variable for updating records
   this.studname=name;
   this.studgender=gender;
   this.studDept=dept;
   this.studDesc=desc;
-
   this.uid=id;
-
 }
-
-updateaddStudent(form:NgForm) { 
-  this.show=true;
-  this.studata[this.uid-1].name=this.studname;
-  this.studata[this.uid-1].gender=this.studgender;
-  this.studata[this.uid-1].desc=this.studDesc;
-  this.studata[this.uid-1].dept=this.studDept;
-  console.log(this.studata);
-  form.reset();
-}
-
 }
 
